419f91e42d8db247576484707fe5a8dac58b45c9
ahyoon99 <kimay9623@naver.com>
2023-07-28 00:36:44 +0900

    [상품 수정] POJO 상품 수정 기능 구현 - code refactoring(Mockito)

--- src/test/java/com/example/productorderservice/product/ProductServiceTest.java
+++ src/test/java/com/example/productorderservice/product/ProductServiceTest.java
@@ -2,17 +2,22 @@ package com.example.productorderservice.product;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+@ExtendWith(MockitoExtension.class)
 public class ProductServiceTest {
 
     private ProductService productService;
 
-    private StubProductPort productPort = new StubProductPort();
+    private ProductPort productPort;
 
     @BeforeEach
     void setUp() {
+        productPort = Mockito.mock(ProductPort.class);
         productService = new ProductService(productPort);
     }
 
@@ -22,7 +27,7 @@ public class ProductServiceTest {
         final UpdateProductRequest request = new UpdateProductRequest("상품 수정", 2000, DiscountPolicy.NONE);
         final Product product = new Product("상품명", 1000, DiscountPolicy.NONE);
 
-        productPort.getProduct_will_return = product;
+        Mockito.when(productPort.getProduct(productId)).thenReturn(product);
 
         productService.updateProduct(productId, request);
 
@@ -30,18 +35,18 @@ public class ProductServiceTest {
         assertThat(product.getPrice()).isEqualTo(2000);
     }
 
-    private static class StubProductPort implements ProductPort {
-
-        public Product getProduct_will_return;
-
-        @Override
-        public void save(Product product) {
-
-        }
-
-        @Override
-        public Product getProduct(Long productId) {
-            return getProduct_will_return;
-        }
-    }
+//    private static class StubProductPort implements ProductPort {
+//
+//        public Product getProduct_will_return;
+//
+//        @Override
+//        public void save(Product product) {
+//
+//        }
+//
+//        @Override
+//        public Product getProduct(Long productId) {
+//            return getProduct_will_return;
+//        }
+//    }
 }
